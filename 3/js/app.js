3730d91b97f736690e26268470f7a38c38a61d6e
Alexandr Kuzovkov <G:\Users\Alexander\AppData\Roaming\The Bat!>
2015-03-07 21:56:17 +0300

    next

--- 3/js/app.js
+++ 3/js/app.js
@@ -12,12 +12,11 @@ var App =
 	mapIdDom: 'map',
 	center: [55.76, 37.64],
 	zoom: 10,
-	storageKey: 'myapp',
 	markers: null,
 	
 	init: function(){
 		this.elements.list = document.getElementById('list');
-		this.elements.dropZone = document.getElementById('drop_zone');
+		this.elements.dropZone = document.getElementById('map');
 		this.elements.place = document.getElementById('user-place');
 		this.elements.descr = document.getElementById('descr');
 		this.elements.lat = document.getElementById('lat');
@@ -25,26 +24,40 @@ var App =
 		document.getElementById('add-form').onsubmit = this.addMarker;
 		YandexMapService.setService(ymaps);
 		YandexMapService.showMap(this.mapIdDom, this.center, this.zoom);
-		var savedData = Storage.load(this.storageKey);
-		if ( savedData ) this.markers = JSON.parse(savedData);
-		Geo.getPosition(this.applyGeoInfo, this.noGeoInfo);
+		var savedMarkers = this.restoreMarkers();
+		if ( savedMarkers ) this.markers = JSON.parse(savedMarkers);
+		var savedCenter = this.restoreCenter();
+		if ( savedCenter ){
+			this.center = JSON.parse(savedCenter);
+		} else{
+			Geo.getPosition(this.applyGeoInfo, this.noGeoInfo);
+		}
+		this.start();
 	},
 	
 	applyGeoInfo: function(center){
 		YandexMapService.moveMap(center, App.zoom);
-		App.elements.place.innerHTML = 'Широта: ' + center[0] + '; Долгота: ' + center[1];
+		
+		App.center = center;
+		App.saveCenter();
+	},
+	
+	start: function(){
+		App.elements.place.innerHTML = 'Широта: ' + this.center[0] + '; Долгота: ' + this.center[1];
+		YandexMapService.moveMap(this.center, this.zoom);
 		if ( App.markers != null ) App.updateMarkers();
+		YandexMapService.addMarker()
 	},
 	
 	noGeoInfo: function(errorMsg){
-		App.elements.place.innerHTML = 'Неопределено';
-		alert(errorMsg);
+		App.showMsg(errorMsg);
 	},
 	
 	updateMarkers: function(){
 		var center = [];
 		var text = '';
 		YandexMapService.deleteMarkers();
+		YandexMapService.addMarker(App.center,'Вы здесь!')
 		for ( var i = 0; i < this.markers.length; i++ ){
 			center = [this.markers[i].coordinates.latitude,this.markers[i].coordinates.longitude];
 			text = this.markers[i].name;
@@ -56,10 +69,10 @@ var App =
 	readDataFromFile: function(data){
 		try{
 			this.markers = JSON.parse(data);
-			Storage.save(this.storageKey, JSON.stringify(this.markers));
-			
+			this.saveMarkers();
 		}catch(e){
-			alert("Wrong file format!");
+			App.showMsg("Wrong file format!");
+			return;
 		}
 		this.updateMarkers();
 		
@@ -72,23 +85,32 @@ var App =
 		}
 		content += '</ul>';
 		App.elements.list.innerHTML = content;
+		var imgs = document.getElementsByClassName('del-img');
+		for ( var i = 0; i < imgs.length; i++ ){
+			imgs[i].addEventListener('click', App.handlerClickDelete,false);
+		}
+		
+		//console.log(content);
 	},
+	
+	handlerClickDelete: function(e){
+		App.delMarker(parseInt(this.id.split('-').pop()));
+	}, 
 	
 	marker2string: function(i){
 		var str = '';
 		str += '<li>'; 
-		str += App.markers[i].id + '<span>' + App.markers[i].name + '</span>';
-		str += '<span>' + JSON.stringify([App.markers[i].coordinates.latitude,App.markers[i].coordinates.longitude]) + '</span>';
-		str += '</li>';
+		str += '<span class="id">' + App.markers[i].id + '</span>' + '<span class="name">' + App.markers[i].name + '</span>';
+		str += '<span class="coord">' + JSON.stringify([App.markers[i].coordinates.latitude,App.markers[i].coordinates.longitude]) + '</span>';
+		str += '<span class="del"><img class="del-img" id="del-' + App.markers[i].id + '" title="Удалить" src="img/delete.png"/></span></li>';
 		return str;
 	},
 	
 	addMarker: function(e){
 		e.preventDefault();
 		var id = null;
-		for ( var i = 0; i < App.markers[i]; i++ ){
+		for ( var i = 0; i < App.markers.length; i++ ){
 			if ( id == null ) id = App.markers[i].id;
-			console.log(App.markers[i].id);
 			if ( id < App.markers[i].id ) id = App.markers[i].id;
 		}
 		id++;
@@ -96,11 +118,40 @@ var App =
 		var lng = parseFloat(App.elements.lng.value);
 		App.markers.push({id:id, name:App.elements.descr.value, coordinates:{latitude:lat, longitude:lng}});
 		App.updateMarkers();
+		App.saveMarkers();
 	
+	},
+	
+	delMarker: function(id){
+		for ( var i = 0; i < this.markers.length; i++ ){
+			if ( this.markers[i].id == id ){
+				delete this.markers[i];
+				this.markers.splice(i,1);
+			} 
+		}
+		this.updateMarkers();
+		this.saveMarkers();
+	},
+	
+	showMsg: function(msg){
+		alert(msg);
+	},
+	
+	saveMarkers: function(){
+		Storage.save('markers', JSON.stringify(this.markers));
+	},
+	
+	restoreMarkers: function(){
+		return Storage.load('markers');
+	},
+	
+	saveCenter: function(){
+		Storage.save('center', JSON.stringify(this.center));
+	},
+	
+	restoreCenter: function(){
+		return Storage.load('center');
 	}
-	
-	
-	
 	
 	
 
